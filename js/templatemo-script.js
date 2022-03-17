99892399f3ef5217b07042e7df8f2044e36706da
Chante Blais <chante.blais@kleientertainment.com>
2022-03-16 19:48:06 -0700

    update fade

--- js/templatemo-script.js
+++ js/templatemo-script.js
@@ -2,7 +2,7 @@ const initBg = (autoplay = true) => {
     const bgImgsNames = ['purple.jpg', 'purple2.jpg', 'purple3.jpg'];
     const bgImgs = bgImgsNames.map(img => "img/" + img);
 
-    $.backstretch(bgImgs, {duration: 10000, fade: 4000});
+    $.backstretch(bgImgs, {duration: 10, fade: 10000});
 
     if(!autoplay) {
       $.backstretch('pause');  
